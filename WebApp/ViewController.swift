122a48ede193dfe25e5bf776da1fcd466f4c9d8b
gaidaasd00 <swiftgaida@list.ru>
2022-09-11 20:48:19 +0300

    1. groped webKit

--- WebApp/ViewController.swift
+++ WebApp/ViewController.swift
@@ -1,17 +1,41 @@
 //
-//  ViewController.swift
-//  WebApp
-//
-//  Created by Алексей Гайдуков on 11.09.2022.
-//
-
+//  ViewController.swiftº
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    //on javaScript
+    let webView: WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = prefs
+        let webView = WKWebView(
+            frame: .zero,
+            configuration: configuration
+        )
+        return webView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(webView)
+        
+        guard let url = URL(string: "https://habr.com/ru/all/") else { return }
+        webView.load(URLRequest(url: url))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+5) { [unowned self] in
+            webView.evaluateJavaScript("document.body.innerHTML") { result, error in
+                guard let html = result as? String, error == nil else { return }
+                print(html)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
     }
 
 
